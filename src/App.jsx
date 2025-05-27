4063a18bfd2713606c109de21d491f82d5f016a2
MohammadLab <moe.labak@gmail.com>
2025-05-27 01:09:32 -0400

    Add header with site name and categories dropdown to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ProductCategory from './pages/ProductCategory';
-import AllProducts from './pages/AllProducts';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductCategory from "./pages/ProductCategory";
+import AllProducts from "./pages/AllProducts";
+import Header from "./components/Header"; // ✅ Import the Header
+
 export default function App() {
   return (
     <Router>
+      <Header /> {/* ✅ Add the header above the routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:type" element={<ProductCategory />} />
