c5ce54a97810267b0e8f80a50f65d10d6ac852d2
Mohammad Labak <moe.labak@gmail.com>
2025-05-29 20:12:34 -0400

    Delete src/data directory

--- src/data/products.js
+++
@@ -1,9 +0,0 @@
-export const products = {
-  sofas: [
-    { name: 'Andalusia', folder: 'andalusia', description: 'Carved wood and Damascene-inspired cushions.' },
-    { name: 'Leon', folder: 'leon' }
-  ],
-  stands: [
-    { name: 'Damask', folder: 'damask', description: 'Wooden TV stand with geometric Arabesque carvings.' }
-  ]
-};
